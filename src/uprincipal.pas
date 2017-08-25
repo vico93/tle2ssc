3b80ab9089537d658b882440f9f16ccaad5d39ff
Vinícius Pontin <vpontin93@gmail.com>
2017-08-25 20:25:49 -0300

    First commit

---
+++ src/uprincipal.pas
@@ -0,0 +1,31 @@
+unit uPrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    titulo2: TLabel;
+    titulo1: TLabel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
